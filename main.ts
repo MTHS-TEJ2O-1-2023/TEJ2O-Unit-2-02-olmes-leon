9ee762eb9a82cccc2529e30d0f3bf2fd4fc93dfa
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-09-15 12:49:01 -0400

    fixd

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Olmes
+ * Created on: Sep 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
